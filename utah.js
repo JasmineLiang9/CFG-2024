381e765d3e4faa1a088d7f8ddd7ab80c7ddeb816
LouiseLow <159493331+LouiseLow@users.noreply.github.com>
2024-08-02 12:08:07 +0100

    Update utah.js

--- utah.js
+++ utah.js
@@ -1,3 +1,5 @@
+//this let's us click through the tabs on the left hand column//
+
 function showTab(index) {
 
     // Retrieve tab content elements
@@ -14,4 +16,42 @@ function showTab(index) {
     // Add the "active" class to the tab 
     // content element at the specified index
     tabContents[index].classList.add("active");
+}
+
+const slides = document.querySelectorAll(".slides img");
+let slideIndex = 0;
+let intervalId = null;
+
+document.addEventListener("DOMContentLoaded", initializeSlider);
+
+function initializeSlider(){
+    if(slides.length > 0){
+        slides[slideIndex].classList.add("displaySlide");
+        intervalId = setInterval(nextSlide, 5000);
+    }
+}
+
+function showSlide(index){
+    if(index >= slides.length){
+        slideIndex = 0;
+    }
+    else if(index < 0){
+        slideIndex = slides.length - 1;
+    }
+
+    slides.forEach(slide => {
+        slide.classList.remove("displaySlide");
+    });
+    slides[slideIndex].classList.add("displaySlide");
+}
+
+function prevSlide(){
+    clearInterval(intervalId);
+    slideIndex--;
+    showSlide(slideIndex);
+}
+
+function nextSlide(){
+    slideIndex++;
+    showSlide(slideIndex);
 }
